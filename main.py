87902b8e7c3302c252cd89d34a95bd69565e393e
Dinley <32085290+Dinley@users.noreply.github.com>
2018-03-09 13:12:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import sys
+import pygame, sys
 Gravity = 1
 Speed = 1
 State = 0
 Score = 0
 Level = 0
 Tiles = 0
+def Mainmenu:
+ Print("Welcome to Stayt")
+ menu = Input("Please enter which menu you want to go to")
+
+def Controlmenu:
+  Print("These are the controls")
+  Print("Arrows key are to move up, down, left, and right")
+  Print("Press A in order to change to Solid state")
+  Print("Press S in order to change to Liquid state")
+  Print("Press D in order to change to Gas state")
+  Print("Press enter in order to Pause the game")
+  Print("
