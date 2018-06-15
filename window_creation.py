a089d06eeb10a69560ce1ad1dda084a8b6763be4
spagbolandbolognese <36753146+spagbolandbolognese@users.noreply.github.com>
2018-06-15 13:19:31 +1000

    Add files via upload
    if you run this, you will currently have to quit terminal because it never closes

---
+++ window_creation.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3.5m
+
+import pygame
+
+# Screen dimensions
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+while True :
+    pygame.init()
+
+    size = [SCREEN_WIDTH, SCREEN_HEIGHT]
+    screen = pygame.display.set_mode(size)
