683e6d049a95a7be9d61a88198b6acc674df71f2
spagbolandbolognese <36753146+spagbolandbolognese@users.noreply.github.com>
2018-06-19 10:33:41 +1000

    Update window_creation.py

--- window_creation.py
+++ window_creation.py
@@ -2,12 +2,33 @@
 
 import pygame
 
-# Screen dimensions
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
+pygame.init()
 
-while True :
-    pygame.init()
+def create_window () :
+    # Screen dimensions
+    SCREEN_WIDTH = 1000
+    SCREEN_HEIGHT = 800
+    
+    done = False
 
-    size = [SCREEN_WIDTH, SCREEN_HEIGHT]
-    screen = pygame.display.set_mode(size)
+    while done == False :
+        pygame.init()
+        
+        #screen details
+        size = [SCREEN_WIDTH, SCREEN_HEIGHT]
+        screen = pygame.display.set_mode(size)
+        pygame.display.set_caption("Stayt")
+        
+        #closing the window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            
+            #closing game through escape key
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    print('You hit escape')
+                    print('That quits the game')
+                    done = True
+        
+    return
