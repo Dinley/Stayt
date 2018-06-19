79bb2a2a0fd2f3b0a8cd657e0d3d3df8fa876290
spagbolandbolognese <36753146+spagbolandbolognese@users.noreply.github.com>
2018-06-19 10:33:02 +1000

    Update key_presses.py

--- key_presses.py
+++ key_presses.py
@@ -6,7 +6,7 @@ pygame.init()
 
 done = False
 
-
+#detect key presses
 def pressed_keys() :
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -19,5 +19,12 @@ def pressed_keys() :
             if event.key == pygame.K_DOWN:
                 print('you hit down')
                 
+            #closing game
+            if event.key == pygame.K_ESCAPE:
+                    print('You hit escape')
+                    print('That quits the game')
+                    quit()
+
+#loop key press detection
 while done == False :
     pressed_keys()
