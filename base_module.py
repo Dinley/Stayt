9e143722cd3daf89776655caaa5f456c8b60c1c3
spagbolandbolognese <36753146+spagbolandbolognese@users.noreply.github.com>
2018-06-29 13:08:00 +1000

    Update base_module.py

--- base_module.py
+++ base_module.py
@@ -1,16 +1,9 @@
 #!/usr/local/bin/python3.5m
 
 import pygame
-#import window_creation
 import key_presses
 
-#import pygame
-
 pygame.init() 
-
-#window_creation.create_window()
-#key_presses.pressed_keys()
-
 
 def main () :
     
@@ -49,29 +42,3 @@ def main () :
         #key_presses.pressed_keys()
         
 main()
-
-"""
-#Screen creation
-    SCREEN_WIDTH = 1000
-    SCREEN_HEIGHT = 800
-    
-    #screen details
-    size = [SCREEN_WIDTH, SCREEN_HEIGHT]
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Stayt")
-    
-    done = False
-    
-    while done == False :
-        
-        #closing the window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quit()
-            
-            #closing game through escape key
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    print('You hit escape')
-                    print('That quits the game')
-                    done = True"""
